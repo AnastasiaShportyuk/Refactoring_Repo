dfea20db4f6010705f5e118a87c76e2ec24ab405
AnastasiaShportyuk <animaska29@gmail.com>
2017-05-29 22:57:05 +0300

    Фиксация №2 - Рефакторинг "Замена значения ссылкой"

--- Refactoring_Shportyuk/Customer.cs
+++ Refactoring_Shportyuk/Customer.cs
@@ -1,60 +1,88 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
+using System.Collections.Generic;
+using System.Collections;
 
 namespace Refactoring_Shportyuk
 {
     //Замена значения ссылкой
     class Customer
     {
+        //Хранение  с помощью статического поля ( Предоставление доступа к экземплярам класса покупателя) Реестр, который содержит пул всех обьектов ссылок 
+        //и получать нужные экземпляры из него (Позволит сделать несколько позиций в обьекте заказе)
+        private static Hashtable instances = new Hashtable();
 
         public string Name
         {
             get;
-            set;
+            private set; //Запрет изменения имени покупателя, т.к. он создается заранее
         }
 
-        public Customer(string name)
+        private Customer(string name)
         {
             this.Name = name;
         }
 
-
-        public class Order
+        //Модифицируем чтобы он возвращал заранее созданного покупателя
+        public static Customer GetByName(string name)
         {
-            private Customer customer;
-
-            public string CustomerName
-            {
-                get { return customer.Name; }
-                set { customer.Name = value; }
-            }
-
-            public Order(string customerName)
-            {
-                this.customer = new Customer(customerName);
-            }
+            return (Customer)instances[name];
+        }
+        
+        //Создаем покупателей заранее ( Загружаем тех клиентов, которые находятся в работе, можно брать из базы данных или файла)
+        public static void LoadCustomers()
+        {
+            new Customer("Компания №1").Store();
+            new Customer("Компания №2").Store();
+            new Customer("Компания №3").Store();
+        }
+        private void Store()
+        {
+            instances.Add(this.Name, this);
         }
 
+        //CustomerName убираем так как сеттер стал приватным и извне доступен только для чтения, убираем возможность его установки.
+    
 
-        private static int NumberOfOrdersFor(List<Order> orders, string customer)
+    // Пропала возможность динамически устанавливать покупателя для заказа по его имени (Добавим новый метод Order)
+    public class Order
+    {
+        
+        private Customer customer;
+
+        public string CustomerName
         {
-            int result = 0;
+            get { return customer.Name; }
+        }
 
-            if (orders != null)
+        public Order(string customerName)
+        {
+            SetCustomer(customerName);
+        }
+        //Выделим код который уже используется в конструкторе заказа в новый метод
+        public void SetCustomer(string customerName)
+        {
+            customer = Customer.GetByName(customerName);
+        }
+    }
+
+    private static int NumberOfOrdersFor(List<Order> orders, string customer)
+    {
+        int result = 0;
+
+        if (orders != null)
+        {
+            foreach (Order order in orders)
             {
-                foreach (Order order in orders)
+                if (string.Equals(order.CustomerName, customer))
                 {
-                    if (string.Equals(order.CustomerName, customer))
-                    {
-                        result++;
-                    }
+                    result++;
                 }
             }
-
-            return result;
         }
 
+        return result;
+    }
         //Извлечение метода
         void PrintOrders(string customer)
         {
